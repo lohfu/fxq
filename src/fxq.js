25823a7008ce34d3cf96e481040256f911bf8687
Linus Miller <lohfu@lohfu.io>
2016-04-23 16:28:46 +0200

    Retab.

--- src/fxq.js
+++ src/fxq.js
@@ -1,98 +1,98 @@
 // Copied from jQuery 2.2.1 (i think) 2016-03-05. And then simplified.
 
-var queues = new Map(),
-	hooks = new Map();
+const queues = new Map(),
+  hooks = new Map();
 
 module.exports = {
-	finish: function(elem) {
-		var queue = queues.get(elem) || [],
-			hooks = this._queueHooks(elem);
+  finish: function(elem) {
+    var queue = queues.get(elem) || [],
+      hooks = this._queueHooks(elem);
 
-		// Empty the queue first
-		this.queue(elem, []);
+    // Empty the queue first
+    this.queue(elem, []);
 
-		if (hooks && hooks.stop) {
-			hooks.stop.call(this, true);
-		}
-	},
+    if (hooks && hooks.stop) {
+      hooks.stop.call(this, true);
+    }
+  },
 
-	queue: function(elem, data) {
-		var queue;
+  queue: function(elem, data) {
+    var queue;
 
-		if (elem) {
-			queue = queues.get(elem);
+    if (elem) {
+      queue = queues.get(elem);
 
-			// Speed up dequeue by getting out quickly if this is just a lookup
-			if (data) {
-				if (!queue || _.isArray(data)) {
-					// TODO better makeArray
-					queue = _.isArray(data) ? data : [ data ];
-					queues.set(elem, queue);
-				} else {
-					queue.push(data);
-				}
-			}
+      // Speed up dequeue by getting out quickly if this is just a lookup
+      if (data) {
+        if (!queue || _.isArray(data)) {
+          // TODO better makeArray
+          queue = _.isArray(data) ? data : [ data ];
+          queues.set(elem, queue);
+        } else {
+          queue.push(data);
+        }
+      }
 
-				// Ensure a hooks for this queue
-			this._queueHooks( elem );
+        // Ensure a hooks for this queue
+      this._queueHooks( elem );
 
-			if (queue[0] !== "inprogress") {
-				this.dequeue(elem);
-			}
+      if (queue[0] !== "inprogress") {
+        this.dequeue(elem);
+      }
 
-			return queue || [];
-		}
-	},
+      return queue || [];
+    }
+  },
 
-	dequeue: function(elem, type) {
-		var self = this;
+  dequeue: function(elem, type) {
+    var self = this;
 
-		var queue = queues.get(elem) || [],
-			startLength = queue.length,
-			fn = queue.shift(),
-			hooks = this._queueHooks(elem),
-			next = function() {
-				self.dequeue(elem);
-			};
+    var queue = queues.get(elem) || [],
+      startLength = queue.length,
+      fn = queue.shift(),
+      hooks = this._queueHooks(elem),
+      next = function() {
+        self.dequeue(elem);
+      };
 
-		// If the fx queue is dequeued, always remove the progress sentinel
-		if (fn === "inprogress") {
-			fn = queue.shift();
-			startLength--;
-		}
+    // If the fx queue is dequeued, always remove the progress sentinel
+    if (fn === "inprogress") {
+      fn = queue.shift();
+      startLength--;
+    }
 
-		if (fn) {
-			// Add a progress sentinel to prevent the fx queue from being
-			// automatically dequeued
-			queue.unshift("inprogress");
+    if (fn) {
+      // Add a progress sentinel to prevent the fx queue from being
+      // automatically dequeued
+      queue.unshift("inprogress");
 
-			// Clear up the last queue stop function
-			delete hooks.stop;
+      // Clear up the last queue stop function
+      delete hooks.stop;
 
-			fn.call( elem, next, hooks );
-		}
+      fn.call( elem, next, hooks );
+    }
 
-		if (!startLength && hooks) {
-			hooks.empty();
-		}
-	},
+    if (!startLength && hooks) {
+      hooks.empty();
+    }
+  },
 
-	// Not public - generate a queueHooks object, or return the current one
-	_queueHooks: function(elem) {
-		var hook = hooks.get(elem);
+  // Not public - generate a queueHooks object, or return the current one
+  _queueHooks: function(elem) {
+    var hook = hooks.get(elem);
 
-		if(!hook) {
-			// NOTE: this has changed from jQuery's implementation. they used
-			// empty: $.Callbacks( "once memory" ).add( function() {
-			hook = {
-				empty: function() {
-					queues.delete(elem);
-					hooks.delete(elem);
-				}
-			};
-			hooks.set(elem,  hook);
-		}
+    if(!hook) {
+      // NOTE: this has changed from jQuery's implementation. they used
+      // empty: $.Callbacks( "once memory" ).add( function() {
+      hook = {
+        empty: function() {
+          queues.delete(elem);
+          hooks.delete(elem);
+        }
+      };
+      hooks.set(elem,  hook);
+    }
 
-		return  hook;
-	}
+    return  hook;
+  }
 };
