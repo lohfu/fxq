3c165fb209cc501f26ea5b236cdfdeb5146a3766
Linus Miller <lohfu@lohfu.io>
2016-08-22 15:05:39 +0200

    Convert to ES2015 module

--- src/fxq.js
+++ src/fxq.js
@@ -3,94 +3,91 @@
 const queues = new Map(),
   hooks = new Map();
 
-module.exports = {
-  finish(elem) {
-    const hooks = this._queueHooks(elem);
+export function finish(elem) {
+  const hooks = _queueHooks(elem);
 
-    // Empty the queue first
-    this.queue(elem, []);
+  // Empty the queue first
+  queue(elem, []);
 
-    if (hooks && hooks.stop) {
-      hooks.stop.call(this, true);
-    }
-  },
-
-  queue(elem, data) {
-    let queue;
-
-    if (elem) {
-      queue = queues.get(elem);
-
-      // Speed up dequeue by getting out quickly if this is just a lookup
-      if (data) {
-        if (!queue || Array.isArray(data)) {
-          // TODO better makeArray
-          queue = Array.isArray(data) ? data : [ data ];
-          queues.set(elem, queue);
-        } else {
-          queue.push(data);
-        }
-      }
-
-        // Ensure a hooks for this queue
-      this._queueHooks(elem);
-
-      if (queue[0] !== 'inprogress') {
-        this.dequeue(elem);
-      }
-
-      return queue || [];
-    }
-  },
-
-  dequeue(elem, type) {
-    const queue = queues.get(elem) || [],
-      hooks = this._queueHooks(elem),
-      next = () => {
-        this.dequeue(elem);
-      };
-
-    let startLength = queue.length,
-      fn = queue.shift();
-
-    // If the fx queue is dequeued, always remove the progress sentinel
-    if (fn === 'inprogress') {
-      fn = queue.shift();
-      startLength--;
-    }
-
-    if (fn) {
-      // Add a progress sentinel to prevent the fx queue from being
-      // automatically dequeued
-      queue.unshift('inprogress');
-
-      // Clear up the last queue stop function
-      delete hooks.stop;
-
-      fn.call(elem, next, hooks);
-    }
-
-    if (!startLength && hooks) {
-      hooks.empty();
-    }
-  },
-
-  // Not public - generate a queueHooks object, or return the current one
-  _queueHooks(elem) {
-    let hook = hooks.get(elem);
-
-    if (!hook) {
-      // NOTE: this has changed from jQuery's implementation. they used
-      // empty: $.Callbacks( "once memory" ).add( function() {
-      hook = {
-        empty() {
-          queues.delete(elem);
-          hooks.delete(elem);
-        }
-      };
-      hooks.set(elem, hook);
-    }
-
-    return hook;
+  if (hooks && hooks.stop) {
+    hooks.stop(true);
   }
-};
+}
+
+export function queue(elem, data) {
+  let queue;
+
+  if (elem) {
+    queue = queues.get(elem);
+
+    // Speed up dequeue by getting out quickly if this is just a lookup
+    if (data) {
+      if (!queue || Array.isArray(data)) {
+        // TODO better makeArray
+        queue = Array.isArray(data) ? data : [ data ];
+        queues.set(elem, queue);
+      } else {
+        queue.push(data);
+      }
+    }
+
+      // Ensure a hooks for this queue
+    _queueHooks(elem);
+
+    if (queue[0] !== 'inprogress') {
+      dequeue(elem);
+    }
+
+    return queue || [];
+  }
+}
+
+export function dequeue(elem, type) {
+  const queue = queues.get(elem) || [],
+    hooks = _queueHooks(elem),
+    next = () => {
+      dequeue(elem);
+    };
+
+  let startLength = queue.length,
+    fn = queue.shift();
+
+  // If the fx queue is dequeued, always remove the progress sentinel
+  if (fn === 'inprogress') {
+    fn = queue.shift();
+    startLength--;
+  }
+
+  if (fn) {
+    // Add a progress sentinel to prevent the fx queue from being
+    // automatically dequeued
+    queue.unshift('inprogress');
+
+    // Clear up the last queue stop function
+    delete hooks.stop;
+
+    fn.call(elem, next, hooks);
+  }
+
+  if (!startLength && hooks) {
+    hooks.empty();
+  }
+}
+
+// Not public - generate a queueHooks object, or return the current one
+export function _queueHooks(elem) {
+  let hook = hooks.get(elem);
+
+  if (!hook) {
+    // NOTE: this has changed from jQuery's implementation. they used
+    // empty: $.Callbacks( "once memory" ).add( function() {
+    hook = {
+      empty() {
+        queues.delete(elem);
+        hooks.delete(elem);
+      }
+    };
+    hooks.set(elem, hook);
+  }
+  return hook;
+}
